e3f405ec0ead613e0e8523f33b99659e9ddb4a77
변혜빈 <seramirochia@gmail.com>
2022-01-28 16:22:17 +0900

    tnwjd

--- Daughter&Mother/Assets/Script/Item/Item.cs
+++ Daughter&Mother/Assets/Script/Item/Item.cs
@@ -2,21 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum ItemType 
-{
-    Weapons,
-    Shield, 
-    Consumables
-}
-
 // Serializable attribute 추가
 // 다시 객체를 사용할 수 있도록 객체의 상태를 저장
 [System.Serializable]
 public class Item 
 {
-    public ItemType itemType;
-    public string itemName;
-    public Sprite itemImage;
+    public string itemName; //아이템 이름
+    public string itemDescription; //아이템 설명
+    public Sprite itemIcon; // 아이템 아이콘
+    public ItemType itemType; // 아이템 타입
+    // 아이템 타입 열거 (무기/방어구/체력회복아이템)
+    public enum ItemType
+    {
+    Weapon,
+    Shield, 
+    medicine
+    } 
+
+    // 생성자
+    public Item(string _itemName, string _itemDescription, ItemType _itemType)
+    {
+        itemName = _itemName;
+        itemDescription = _itemDescription;
+        itemType = _itemType;
+        // itemName = itemIcon의 파일 이름임
+        // Resources 폴더의 해당 스프라이트를 가져옴
+        itemIcon = Resources.Load("ItemIcon/ " + _itemName.ToString(), typeof(Sprite)) as Sprite;
+    }
+
 
     // 아이템 사용 여부를 반환하는 메소드
     public bool Use()
